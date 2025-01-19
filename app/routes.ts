4436c4b060c4e63f5f88c1b8d420600034962645
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-01-19 08:24:22 -0500

    Enable flat routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,9 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig } from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
 
-export default [
-  index("routes/home.tsx"),
-  route("foo", "routes/foo.tsx"),
-] satisfies RouteConfig;
+// export default [
+//   index("routes/home.tsx"),
+//   route("foo", "routes/foo.tsx"),
+// ] satisfies RouteConfig;
+
+export default flatRoutes() satisfies RouteConfig;
