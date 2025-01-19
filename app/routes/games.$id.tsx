8015136030f93f31f95e6db7866e10ac6b00ea71
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-01-19 10:16:21 -0500

    Set search params test

--- app/routes/games.$id.tsx
+++ app/routes/games.$id.tsx
@@ -1,7 +1,11 @@
+import { useSearchParams } from "react-router";
 import type { Route } from "./+types/games.$id";
 
-export async function loader({ params }: Route.LoaderArgs) {
+export async function loader({ params, request }: Route.LoaderArgs) {
   console.log("params: ", params);
+  const url = new URL(request.url);
+  const foo = url.searchParams.get("foo");
+  console.log("foo: ", foo);
 
   return {
     id: params?.id,
@@ -9,8 +13,22 @@ export async function loader({ params }: Route.LoaderArgs) {
 }
 
 export default function GameDetails({ loaderData }: Route.ComponentProps) {
+  const [searchParams, setSearchParams] = useSearchParams();
   console.log("loaderData: ", loaderData);
 
-  return <div>DETAILS</div>;
-  //        ^ string
+  function handleParams() {
+    const params = {
+      foo: "bar",
+    };
+    setSearchParams(params);
+  }
+
+  return (
+    <div>
+      <h1>Details</h1>
+      <div>
+        <button onClick={handleParams}>Set test</button>
+      </div>
+    </div>
+  );
 }
