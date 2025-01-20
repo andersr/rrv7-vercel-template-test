f39b44c2ea942a66c526a55e8020c601deb77d9a
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-01-20 14:19:48 -0500

    body styling fix

--- app/root.tsx
+++ app/root.tsx
@@ -33,12 +33,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="prose h-full bg-gradient-to-r from-base-100 to-base-200 py-8 px-4">
+      <body className="h-full bg-gradient-to-r from-base-100 to-base-200 py-8 px-4">
         <div className="mx-auto md:w-1/3 h-full flex flex-col">
-          <main className="prose-h1:text-primary-content container flex-1">
+          <main className="prose prose-h1:text-primary-content container flex-1">
             {children}
           </main>
-          <footer className="text-center text-sm">
+          <footer className="prose text-center text-sm">
             <p className="mb-2">
               This is a simple demo, in the form of a trivia game, of using
               structured output from an AI Assistant.
