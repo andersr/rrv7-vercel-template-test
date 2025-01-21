c86baefa1c2c4d57933ac1f136be83857a5a5ec5
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-01-21 08:54:18 -0500

    Fix blog post link

--- app/root.tsx
+++ app/root.tsx
@@ -48,7 +48,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 Code Repo
               </a>{" "}
               |{" "}
-              <a href="https://github.com/andersr/structured-response-demo">
+              <a href="https://www.anders.co/blog/creating-app-features-using-ai-assistant-structured-output/">
                 Blog Post
               </a>
             </p>
