5198085e19adcec345dda73003f2faf40b0dc963
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-01-29 07:54:27 -0500

    cleanup

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,4 @@
 import { type RouteConfig } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
-// export default [
-//   index("routes/home.tsx"),
-//   route("foo", "routes/foo.tsx"),
-// ] satisfies RouteConfig;
-
 export default flatRoutes() satisfies RouteConfig;
